7422a30f5528bdd322d7991a62fa196d3e68c6d8
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2024-03-22 14:54:15 -0300

    Atualizar dependências do axios e corrigir importações de componentes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,9 +5,9 @@ import { Component } from "react";
 import SpendWiseAPI from "@/api";
 
 import ExpenseItem from "@/components/ExpenseItem/ExpenseItem";
-import Button from "@/components/Button/Button";
-import Input from "@/components/Input/Input";
-import Select from "@/components/Select/Select";
+import Button from "@/components/button/Button";
+import Input from "@/components/input/Input";
+import Select from "@/components/select/Select";
 import Header from "@/components/Header/Header";
 
 export default class Home extends Component {
